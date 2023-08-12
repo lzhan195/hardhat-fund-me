e2e37bdb21e8c13bcd001ea2b1da2e15eec07a7e
lzhan195 <longtengzhang77@gmail.com>
2023-08-11 23:24:47 -0400

    mocking and deploy scripts

---
+++ contracts/FundMe.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+//Pramga
+pragma solidity ^0.8.8;
+//Imports
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+//Error Codes
+error FundMe_NotOwner();
+
+//Interfaces, Libraries, Contracts
+
+/**@title A sample Funding Contract
+ * @author CZ
+ * @notice This contract is for creating a sample funding contract
+ * @dev This implements price feeds as our library
+ */
+contract FundMe {
+    //Type Declarations
+    using PriceConverter for uint256;
+    //State Variables
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    address public immutable i_owner;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+    
+    AggregatorV3Interface public priceFeed;
+    //Modifiers
+    modifier onlyOwner {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert FundMe_NotOwner();
+        _;
+    }
+
+    // Functions Order:
+    //// constructor
+    //// receive
+    //// fallback
+    //// external
+    //// public
+    //// internal
+    //// private
+    //// view / pure
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    // receive() external payable {
+    //     fund();
+    // }
+
+    // fallback() external payable {
+    //     fund();
+    // }
+
+    /**
+    * @notice This function funds this contract
+    * @dev this implements price feeds as our library
+     */
+    function fund() public payable {
+        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+    
+    // function getVersion() public view returns (uint256){
+    //     // ETH/USD price feed address of Sepolia Network.
+    //     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    //     return priceFeed.version();
+    // }
+    
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \ 
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback() 
+    //     /   \ 
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+}
+
---
+++ hardhat.config.js
@@ -0,0 +1,49 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
+
+
+
+const COINMARKEYCAP_API_KEY = process.env.COINMARKEYCAP_API_KEY
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    //    solidity: "0.8.8",
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }]
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    etherscan: { apiKey: ETHERSCAN_API_KEY },
+    gasReporter: {
+        enabled: false,
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKEYCAP_API_KEY,
+        token: "MATIC",
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+}
